610778d88d8435abc8ffb7f8c583dc7f406aec45
FreshyMc <12915203+FreshyMc@users.noreply.github.com>
2026-02-15 12:39:04 +0200

    Project root commands enhancement

--- packages/server/src/models/user.model.ts
+++ packages/server/src/models/user.model.ts
@@ -16,6 +16,7 @@ const userSchema = new Schema(
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     username: { type: String, required: true, unique: true, trim: true },
     passwordHash: { type: String, required: true },
+    passwordSalt: { type: String, required: true },
     role: {
       type: String,
       enum: Object.values(UserRole),
