c99e661728bba007b44f30a5cdbde5ed84f33a2f
FreshyMc <12915203+FreshyMc@users.noreply.github.com>
2026-02-20 21:24:30 +0200

    Add ability to view already approved questions and also manage them

--- packages/client/src/pages/AdminQuestionsPage.tsx
+++ packages/client/src/pages/AdminQuestionsPage.tsx
@@ -1,5 +1,11 @@
 import { ModerationQueue } from '../components/ModerationQueue';
+import { ApprovedQuestionManager } from '../components/ApprovedQuestionManager';
 
 export function AdminQuestionsPage() {
-  return <ModerationQueue />;
+  return (
+    <div className="space-y-4">
+      <ModerationQueue />
+      <ApprovedQuestionManager />
+    </div>
+  );
 }
